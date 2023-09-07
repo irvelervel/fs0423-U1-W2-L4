818a83ca179fcadeeba3479156f273cc05352ba5
Stefano Casasola <ste.casasola@gmail.com>
2023-09-07 12:31:08 +0200

    Parameters and return values

--- functions.js
+++ functions.js
@@ -61,3 +61,70 @@ function createString() {
 // createString() // ora la lancio
 
 alert(createString()) // ESEGUIRE CREATESTRING() EQUIVALE A OTTENERE IL SUO VALORE DI RITORNO (il certificato)
+
+function tornaCinque() {
+  return 5
+}
+
+console.log(tornaCinque() + 5) // 10 :)
+
+// PARAMETRI DI UNA FUNZIONE
+
+function addTwoNumbers(n1, n2) {
+  if (typeof n1 === 'number' && typeof n2 === 'number') {
+    let result = n1 + n2
+    return result
+  } else {
+    return 'Guarda che non stai passando numeri alla funzione!'
+  }
+}
+
+console.log('La somma dei due numeri è', addTwoNumbers(7, 8))
+console.log('La somma dei due numeri è', addTwoNumbers(6, 13))
+console.log('La somma dei due numeri è', addTwoNumbers('9', '12'))
+
+// i nomi che date ai parametri sono di vostra scelta, cercate di darne di sensati
+// non c'è limite a quanti parametri una funziona possa accettare, solo il vostro buonsenso
+// i parametri possono essere di qualunque tipo, potete invocare una funzione passandoci numeri, stringhe, oggetti
+
+// n3, in caso non venga fornito, non diventa più undefined ma diventa 0
+function addThreeNumbers(n1, n2, n3 = 0) {
+  // PASSAGGI COMMENTATI PER TRASFORMARE n1, n2, n3 da eventualmente stringhe a numeri,
+  // ora superflui perchè convertiamo i prompt direttamente un numeri più in sotto
+  //   // parseInt('5') --> 5
+  //   // n1, n2, n3 potrebbero essere stringhe! perchè invoco la funzione con i valori dei prompt
+  //   let convertedNumber1 = typeof n1 === 'number' ? n1 : parseInt(n1) // parseInt converte una stringa a un numero
+  //   // ora sono sicuro al 100% che convertedNumber1 è UN NUMERO!
+  //   let convertedNumber2 // stessa cosa ma con if/else
+  //   if (typeof n2 === 'number') {
+  //     convertedNumber2 = n2
+  //   } else {
+  //     convertedNumber2 = parseInt(n2) // prendi n2 come stringa e trasformalo nel suo corrispondente valore numerico
+  //   }
+  //   let convertedNumber3 = typeof n3 === 'number' ? n3 : parseInt(n3) // parseInt converte una stringa a un numero
+
+  //   // a questo punto ho convertedNumber1, 2 e 3 che sono tutti numeri al 100%
+  return n1 + n2 + n3
+}
+
+console.log(addThreeNumbers(6, 13, 94))
+console.log(addThreeNumbers(6, 13)) // PROBLEMA! Senza il valore di default di n3 nella funzione otterremmo Not A Number,
+// ma ora che abbiamo un valore di default per n3 = 0 la funzione continua a tornare sempre un numero
+
+// ESEMPIO CON PROMPT
+
+const numero1 = parseInt(prompt('Primo numero?')) // prompt() ritorna una stringa, ma lo facciamo passare per parseInt() così
+const numero2 = parseInt(prompt('Secondo numero?')) // da avere un numero in formato numerico,
+const numero3 = parseInt(prompt('Terzo numero?')) // in modo da poterlo fornire come argomento a addThreeNumbers()
+console.log(numero1, numero2, numero3)
+
+console.log(addThreeNumbers(numero1, numero2, numero3))
+
+let x1 = 10
+console.log(x1)
+
+const esempio = function () {
+  console.log('BAU')
+}
+
+esempio()
